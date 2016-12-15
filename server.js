a0d2bc767839e198bd87e0ce9982952f73a6a55c
Becky <yrstudio9316@gmail.com>
2016-12-15 09:18:38 +0800

    Refactor middleware

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ var express = require("express");
 var app = express(); //建立Express個體
 const PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log("private route hit");
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log("Request: " + new Date().toString() + " " + req.method + " " + req.originalUrl);
-        next();
-    }
-};
+var middleware = require("./middleware.js");
+// var middleware = {
+//     requireAuthentication: function (req, res, next) {
+//         console.log("private route hit");
+//         next();
+//     },
+//     logger: function (req, res, next) {
+//         console.log("Request: " + new Date().toString() + " " + req.method + " " + req.originalUrl);
+//         next();
+//     }
+// };
 
 // app.get("/", function (req, res) {
 //     res.send("Hello Express!");
